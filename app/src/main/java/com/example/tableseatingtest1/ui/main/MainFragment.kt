13dd37e9368501b320ab3ca0bc3e3cb5fb584377
christian garillo <garillo.cl@gmail.com>
2020-09-14 12:45:56 +0800

    Remove Dynamic table due to time constraint.

--- app/src/main/java/com/example/tableseatingtest1/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/tableseatingtest1/ui/main/MainFragment.kt
@@ -1,12 +1,9 @@
 package com.example.tableseatingtest1.ui.main
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -33,10 +30,6 @@ class MainFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_customerListFragment)
         )
 
-        createTableButtons(requireContext(), "Table 1")
-        createTableButtons(requireContext(), "Table 2")
-        createTableButtons(requireContext(), "Table 3")
-        createTableButtons(requireContext(), "Table 4")
         return binding.root
     }
 
@@ -45,12 +38,4 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
-    fun createTableButtons(context: Context, btnText: String) {
-        val myButton = Button(context)
-        myButton.setText(btnText)
-        myButton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        binding.tableListButtons.addView(myButton)
-    }
-
 }
